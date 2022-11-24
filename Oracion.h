cea0d585c5f35b3b63983eb72c834997b9b570ba
Aiverson Castaño T <93091122+iverson18@users.noreply.github.com>
2022-11-24 00:08:46 -0500

    Update Oracion.h

--- Oracion.h
+++ Oracion.h
@@ -1,1 +1,23 @@
+#ifndef Oracion_h
+#define Oracion_h
+#include <string>
+#include "Verbo.h"
+#include "Sujeto.h"
 
+using namespace std;
+
+class Oracion{
+    private:
+        string *palabras;
+        Sujeto *sujeto;
+        Verbo *verbo;
+        string predicado;
+
+    public:
+        Oracion(string);
+        ~Oracion();
+        int numPalabras(string);
+        string obtPredicado(string,string[],int);
+};
+
+#endif
