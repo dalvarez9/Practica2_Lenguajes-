1c224354c1381d1f87302dba193cab3a55421010
Aiverson Castaño T <93091122+iverson18@users.noreply.github.com>
2022-11-24 00:07:18 -0500

    Update Verbo.h

--- Verbo.h
+++ Verbo.h
@@ -1,1 +1,17 @@
+#ifndef Verbo_h
+#define Verbo_h
+#include <string>
 
+using namespace std;
+
+class Verbo{
+    private:
+        string list_verbos;
+        string palabra = "";
+    public:
+        Verbo(string[],int);
+        ~Verbo();
+        string lista_verbos();
+        string getPalabra();
+};
+#endif
