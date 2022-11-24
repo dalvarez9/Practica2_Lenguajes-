b553220642aa3dce4f91520313975ed608c39f63
Aiverson Castaño T <93091122+iverson18@users.noreply.github.com>
2022-11-24 00:09:35 -0500

    Update Sujeto.h

--- Sujeto.h
+++ Sujeto.h
@@ -1,1 +1,18 @@
+#ifndef Sujeto_h
+#define Sujeto_h
+#include <string>
 
+using namespace std;
+
+class Sujeto{
+    private:
+        string palabra = "";
+        string list_sujetos;
+        
+    public:
+        Sujeto(string[],int);
+        ~Sujeto();
+        string lista_Sujetos();
+        string getPalabra();
+};
+#endif
