c40256c502e1b583627a343691aa48cdf6184208
Aiverson Castaño T <93091122+iverson18@users.noreply.github.com>
2022-11-24 00:05:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include "Oracion.h"
+using namespace std;
 
+int main() {
+    string texto;
+    cout << "Ingrese una oracion: ";
+    getline(cin, texto);
+    Oracion *oracion = new Oracion(texto);
+}
